a511bc12376e2fe66a3974d7c19539cc46e16dfa
alejandro_lopez24 <alejandro.lopez24@upr.edu>
2023-02-20 20:50:09 -0400

    Changed rectangle color to random shade of blue

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -49,7 +49,7 @@ void ofApp::drawMode1(vector<float> amplitudes) {
     ofFill();        // Drawn Shapes will be filled in with color
     ofSetColor(256); // This resets the color of the "brush" to white
     ofDrawBitmapString("Rectangle Height Visualizer", 0, 15);
-    ofSetColor(189, 45, 135);
+    ofSetColor(0, 0, ofRandom(0,256));
     ofDrawRectangle(2, ofGetHeight() - 100, 50, amplitudes[0]);
 }
 void ofApp::drawMode2(vector<float> amplitudes) {
